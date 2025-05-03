65d782d63530027e50415ffc165feffdaa97d36c
CarlosFranco97 <127879068+CarlosFranco97@users.noreply.github.com>
2025-05-02 22:33:59 -0500

    Se genera el proyecto

---
+++ src/main/java/com/example/app_roze_backend/AppRozeApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_roze_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRozeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRozeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app_roze_backend/AppRozeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_roze_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRozeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
